634280e235aad583ade1a1140ac4062f75181bf2
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-19 18:40:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+  region="eu-west-2"
+}
+resource "aws_instance" "teraserver"{
+  count="1"
+  ami="ami-084e8c05825742534"
+  instance_type="t2.micro"
+  key_name="Jenkins"
+  subnet_id="subnet-07b2b19da18ebe100"
+  security_groups=["sg-0783cf0495e16d6a1"]
+  tags={
+    Name="terform_server"
+    Env="test"
+  }
+}
