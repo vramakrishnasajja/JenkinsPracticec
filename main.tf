e2f4ad2d36a4a2d60a9dfafe25839c2fb1f718ce
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-19 20:16:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-provider "aws"{
-  region="eu-west-2"
+provider "aws" {
+  profile    = "terraform_user"
+  access_key = "AKIA43CSMBFCTKTTR3RD"
+  secret_key = "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"
+  region     = "eu-west-2"
 }
-resource "aws_instance" "teraserver"{
-  count="1"
-  ami="ami-084e8c05825742534"
-  instance_type="t2.micro"
-  key_name="Jenkins"
-  subnet_id="subnet-07b2b19da18ebe100"
-  security_groups=["sg-0783cf0495e16d6a1"]
-  tags={
-    Name="terform_server"
-    Env="test"
+resource "aws_instance" "teraserver" {
+  count           = "1"
+  ami             = "ami-084e8c05825742534"
+  instance_type   = "t2.micro"
+  key_name        = "Jenkins"
+  subnet_id       = "subnet-07b2b19da18ebe100"
+  security_groups = ["sg-0783cf0495e16d6a1"]
+  tags = {
+    Name = "terform_second_server"
+    Env  = "test"
   }
 }
