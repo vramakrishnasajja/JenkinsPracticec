09d14f93c011f7538cfff7bec48bd15dd1b0dc79
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-19 21:06:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = "AKIA43CSMBFC7LUYH32C"
-  secret_key = "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"
-  region     = "eu-west-2"
+   region     = "eu-west-2"
 }
 resource "aws_instance" "teraserver" {
   count           = "1"
