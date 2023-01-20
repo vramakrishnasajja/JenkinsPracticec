5cd91eccafa9b471eea382d588be8d1743370a5c
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-20 18:49:08 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIA43CSMBFCS5YDTIRG"
-}
-variable "secret_key" {
-  default = "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"
-}
-variable "region" {
-  default = "eu-west-2"
-}
