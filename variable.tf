49c28223360ed0d072dab9a11e1d7ce226c032d9
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-20 18:43:05 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA43CSMBFCS5YDTIRG"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "eu-west-2"
+}
