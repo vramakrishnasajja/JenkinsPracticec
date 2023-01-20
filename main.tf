54c982587c77c618d6ae4692f3b795dcfc147298
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-20 18:19:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 provider "aws" {
-   profile = "terrafom_jenkins"
-   access_key = "AKIA43CSMBFCV2Z3XY4T"
-   secret_key = "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"
-   region     = "eu-west-2"
+  profile    = "default"
+  access_key = "AKIA43CSMBFCS5YDTIRG"
+  secret_key = "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"
+  region     = "eu-west-2"
 }
 resource "aws_instance" "teraserver" {
   count           = "1"
   ami             = "ami-084e8c05825742534"
   instance_type   = "t2.micro"
-  key_name        = "Jenkins"
+  key_name        = "Linux_London"
   subnet_id       = "subnet-07b2b19da18ebe100"
-  security_groups = ["sg-0783cf0495e16d6a1"]
+  security_groups = ["sg-0513eab779ca52a7d"]
   tags = {
-    Name = "terform_second_server"
+    Name = "terform_server"
     Env  = "test"
   }
 }
+
