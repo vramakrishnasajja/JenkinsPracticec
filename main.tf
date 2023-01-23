07187db3f3ce09f75e9eb503ed1f6278caf6fb96
vramakrishnasajja <40911615+vramakrishnasajja@users.noreply.github.com>
2023-01-23 22:12:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  profile    = "default"
-  access_key = "AKIA43CSMBFCS5YDTIRG"
-  secret_key = "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"
+  profile    = "IAM_VRKS"
+  access_key = "AKIA43CSMBFCSUDLW6WU"
+  secret_key = "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"
   region     = "eu-west-2"
 }
 resource "aws_instance" "teraserver" {
   count           = "1"
   ami             = "ami-084e8c05825742534"
   instance_type   = "t2.micro"
-  key_name        = "Linux_London"
+  key_name        = "Server"
   subnet_id       = "subnet-07b2b19da18ebe100"
-  security_groups = ["sg-0513eab779ca52a7d"]
+  security_groups = ["sg-0a29d7d6c30e7b2aa"]
   tags = {
-    Name = "terform_server_London"
+    Name = "terform_server_London_other"
     Env  = "test"
   }
 }
